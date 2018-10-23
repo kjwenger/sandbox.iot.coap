d5c6cf169765010cfdba2630181c979d109e74ec
Klaus Wenger <kjwenger@yahoo.com>
2018-10-23 18:50:30 +0200

    added tradfri code

--- node/ikea_tradfri/node-coap-client/pulse_bulb.js
+++ node/ikea_tradfri/node-coap-client/pulse_bulb.js
@@ -1,9 +1,12 @@
 const debug = require('debug')('ikea:tradfri:pulse_bulb')
-const tradfri = require('node-tradfri').create({
-    coapClientPath: 'coap-client',
-    identity: 'IDENTITY',
-    preSharedKey: process.env.IDENTITY,
-    hubIpAddress: process.env.IP_ADDRESS
+const coap = require('node-coap-client').CoapClient
+
+const hostname = `coaps://${process.env.IP_ADDRESS}:5684`
+debug('hostname:', hostname)
+coap.setSecurityParams(hostname, {
+    psk: {
+        IDENTITY: process.env.IDENTITY
+    }
 })
 const bulb = parseInt(process.argv[2]) || 0
 debug('bulb:', bulb)
@@ -14,17 +17,22 @@ debug('address:', address)
 let brightness = 0
 let increment = 1
 
+const target = `coaps://${process.env.IP_ADDRESS}:5684/15001/${address}`
+debug('target:', target)
+
 const shift = () => {
     brightness += increment
     debug('brightness:', brightness)
     if (brightness >= 255) increment = -1
     else if (brightness <= 0) increment = 1
     debug('increment:', increment)
-    return tradfri.setDeviceState(address, {
-        state: 'on',
-        brightness: brightness
-    })
-        .then(debug)
+    const object = { '3311': [ { '5851': brightness } ] }
+    debug('object:', target)
+    const json = JSON.stringify(object, null, 2)
+    debug('json:', json)
+    const payload = Buffer.from(json, 'utf8');
+    debug('payload:', payload)
+    return coap.request(target, 'put', payload)
         .catch(console.error)
 }
 const loop = () => {
